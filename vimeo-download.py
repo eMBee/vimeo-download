30f707378d531011a189eeb4cd39b52fd108589e
Taylor Bell <taylorbell@gmail.com>
2017-02-24 10:18:49 -0700

    allow the json url & a filename as arguments

--- vimeo-download.py
+++ vimeo-download.py
@@ -1,9 +1,9 @@
 import requests
 import base64
 from tqdm import tqdm
+import sys
 
-
-master_json_url = 'https://178skyfiregce-a.akamaihd.net/exp=1474107106~acl=%2F142089577%2F%2A~hmac=0d9becc441fc5385462d53bf59cf019c0184690862f49b414e9a2f1c5bafbe0d/142089577/video/426274424,426274425,426274423,426274422/master.json?base64_init=1'
+master_json_url = sys.argv[1]
 base_url = master_json_url[:master_json_url.rfind('/', 0, -26) + 1]
 
 resp = requests.get(master_json_url)
@@ -15,7 +15,7 @@ video = content['video'][idx]
 video_base_url = base_url + video['base_url']
 print 'base url:', video_base_url
 
-filename = 'video_%d.mp4' % video['id']
+filename = sys.argv[2] if sys.argv[2] else 'video_%d.mp4' % video['id']
 print 'saving to %s' % filename
 
 video_file = open(filename, 'wb')
