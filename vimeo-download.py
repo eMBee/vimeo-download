61c206e487145ffc4d1b15737a0aed9bca1535f7
paschoaletto <paschoaletto@gmail.com>
2017-04-04 14:32:18 -0400

    download video & audio segmented and encode them in one .mp4 file

--- vimeo-download.py
+++ vimeo-download.py
@@ -2,9 +2,12 @@ import requests
 import base64
 from tqdm import tqdm
 import sys
+import subprocess as sp
+
+FFMPEG_BIN = 'ffmpeg.exe'
 
 master_json_url = sys.argv[1]
-base_url = master_json_url[:master_json_url.rfind('/', 0, -26) + 1]
+base_url = master_json_url[:master_json_url.rfind('/', 0, -26) - 5]
 
 resp = requests.get(master_json_url)
 content = resp.json()
@@ -12,10 +15,12 @@ content = resp.json()
 heights = [(i, d['height']) for (i, d) in enumerate(content['video'])]
 idx, _ = max(heights, key=lambda (_, h): h)
 video = content['video'][idx]
-video_base_url = base_url + video['base_url']
+video_base_url = base_url + 'video/' + video['base_url']
 print 'base url:', video_base_url
 
-filename = sys.argv[2] if sys.argv[2] else 'video_%d.mp4' % video['id']
+#filename = sys.argv[2] + '.mp4' if sys.argv[2] else 'video_%d.mp4' % video['id']
+filename = 'v.mp4'
+video_filename = filename
 print 'saving to %s' % filename
 
 video_file = open(filename, 'wb')
@@ -36,3 +41,43 @@ for segment in tqdm(video['segments']):
 
 video_file.flush()
 video_file.close()
+
+audio = content['audio'][0]
+audio_base_url = base_url + audio['base_url'][3:]
+print 'base url:', audio_base_url
+
+#filename = sys.argv[2] + '.mp3' if sys.argv[2] else 'audio_%d.mp3' % audio['id']
+filename = 'a.mp3'
+audio_filename = filename
+print 'saving to %s' % filename
+
+audio_file = open(filename, 'wb')
+
+init_segment = base64.b64decode(audio['init_segment'])
+audio_file.write(init_segment)
+
+for segment in tqdm(audio['segments']):
+    segment_url = audio_base_url + segment['url']
+    resp = requests.get(segment_url, stream=True)
+    if resp.status_code != 200:
+        print 'not 200!'
+        print resp
+        print segment_url
+        break
+    for chunk in resp:
+        audio_file.write(chunk)
+
+audio_file.flush()
+audio_file.close()
+
+filename = sys.argv[2] + '.mp4' if sys.argv[2] else 'video.mp4'
+
+command = [ FFMPEG_BIN,
+        '-y', # (optional) overwrite output file if it exists
+        '-i', audio_filename,
+        '-i',video_filename,
+        '-acodec', 'copy',
+        '-vcodec', 'copy',
+        filename ]
+
+pipe = sp.Popen( command, stdin=sp.PIPE, stderr=sp.PIPE)
