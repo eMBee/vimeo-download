72e2dc75fb4346d3afe71fbaecbca691300d2524
paschoaletto <paschoaletto@gmail.com>
2017-04-04 18:47:15 -0400

    specify h264 codec to ffmpeg

--- vimeo-download.py
+++ vimeo-download.py
@@ -75,7 +75,7 @@ command = [ FFMPEG_BIN,
         '-i', audio_filename,
         '-i',video_filename,
         '-acodec', 'copy',
-        '-vcodec', 'copy',
+        '-vcodec', 'h264',
         filename ]
 
 pipe = sp.Popen( command, stdin=sp.PIPE, stderr=sp.PIPE)
